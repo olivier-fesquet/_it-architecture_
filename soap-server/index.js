2dc6c23c0dbfc3a1f95dbba6cc2d54b473e880ed
olivier <ollivier.fesquet@cegetel.net>
2020-04-13 11:05:12 +0200

    code serveur

--- soap-server/index.js
+++ soap-server/index.js
@@ -1,3 +1,20 @@
-"use strict";
+var myService = {
+    SoapWebService: {
+        SoapWebService_0: {
+            HelloWorld: function (args) {
+                return { result: 'Hello ' + args.name };
+            }
+        }
+    }
+};
 
-module.exports = require('./lib/soap');
+var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+
+//http server example
+var server = require('http').createServer(function (request, response) {
+    response.end('404: Not Found: ' + request.url);
+});
+
+server.listen(8000);
+
+require('soap').listen(server, '/wsdl', myService, xml);
